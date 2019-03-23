7284e4bcfd21d4f168207e2984a40fa21cc40c02
DrMattsuu <drmattsuu2003@gmail.com>
2019-03-23 16:48:58 +0500

    third zadacha

--- Zadachka.py
+++ Zadachka.py
@@ -3,29 +3,17 @@ import requests
 import sys
 import os
 
-def terminate():
-    """
-    Выход из игры
-    :return:
-    """
-    pygame.quit()
-    sys.exit()
-
-spn = 0.005
-sh, dol = 61.402554, 55.159897
-
 
 def draw_screen():
     response = None
     map_params = {
-            "ll": ','.join([str(sh), str(dol)]),
-            "spn": ','.join([str(spn), str(spn)]),
-            "l": "map"
-        }
+        "ll": ','.join([str(sh), str(dol)]),
+        "spn": ','.join([str(spn), str(spn)]),
+        "l": "map"
+    }
     try:
         map_request = "http://static-maps.yandex.ru/1.x/"
         response = requests.get(map_request, params=map_params)
-
 
         if not response:
             print("Ошибка выполнения запроса:")
@@ -51,18 +39,37 @@ def draw_screen():
 pygame.init()
 screen = pygame.display.set_mode((600, 450))
 
-# Рисуем картинку, загружаемую из только что созданного файла.
-while True:
+sh, dol = 61.402554, 55.159897
+spn = 0.005
+running = True
+pygame.display.flip()
+while running:
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-
-            terminate()
+            running = False
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_PAGEDOWN:
                 spn -= spn / 1.2 if spn >= 0.0001 else 0
             if event.key == pygame.K_PAGEUP:
                 spn += spn / 1.2 if spn <= 15 else 0
+            if event.key == pygame.K_DOWN:
+                dol -= spn / 5
+            if event.key == pygame.K_UP:
+                dol += spn / 5
+            if event.key == pygame.K_LEFT:
+                sh -= spn / 3
+            if event.key == pygame.K_RIGHT:
+                sh += spn / 3
+            if dol >= 180:
+                dol -= 360
+            if dol <= -180:
+                dol += 360
+            if sh >= 90:
+                sh -= 180
+            if sh <= -90:
+                sh += 180
     draw_screen()
     pygame.display.flip()
 
