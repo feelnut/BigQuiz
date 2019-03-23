3a3de334d65e47add4bf2842fae9140387dc8da7
milesha <mi.serbinova@gmail.com>
2019-03-23 15:39:23 +0500

    first zadacha

--- Zadachka.py
+++ Zadachka.py
@@ -1,1 +1,41 @@
-# Empty File
+import pygame
+import requests
+import sys
+import os
+
+response = None
+try:
+    map_request = "http://static-maps.yandex.ru/1.x/?ll=61.402554,55.159897&spn=0.152,0.002&l=map"
+    response = requests.get(map_request)
+
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print(geocoder_request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+except:
+    print("Запрос не удалось выполнить. Проверьте наличие сети Интернет.")
+    sys.exit(1)
+
+# Запишем полученное изображение в файл.
+map_file = "map.png"
+try:
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+except IOError as ex:
+    print("Ошибка записи временного файла:", ex)
+    sys.exit(2)
+
+# Инициализируем pygame
+pygame.init()
+screen = pygame.display.set_mode((600, 450))
+# Рисуем картинку, загружаемую из только что созданного файла.
+screen.blit(pygame.image.load(map_file), (0, 0))
+# Переключаем экран и ждем закрытия окна.
+pygame.display.flip()
+while pygame.event.wait().type != pygame.QUIT:
+    pass
+pygame.quit()
+
+# Удаляем за собой файл с изображением.
+os.remove(map_file)
